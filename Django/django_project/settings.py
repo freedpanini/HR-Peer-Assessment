8f5b16e037ec1f0f5c3db07ceea251cfcef40b6a
Fred Pan <fred.pan@bc.edu>
2022-03-15 14:16:58 -0400

    fixing secret key leak

--- Django/django_project/settings.py
+++ Django/django_project/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ho55p4p=jazi#d#m32yil_ba*vqvia$bcz&-14b1ca9=gyx!5i'
+
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
