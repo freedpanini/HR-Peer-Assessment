fc2cc04de14769aa6c19fa92cf9f3666f7855fa1
Zach Crews <crewsz@bc.edu>
2022-03-21 17:01:48 -0400

    Added teams model

--- Django/courses/models.py
+++ Django/courses/models.py
@@ -7,3 +7,6 @@ class Course(models.Model):
 	year = models.DecimalField(decimal_places=0, blank=False)
 	code = models.CharField(max_length=30, blank=False)
 	num_teams = models.DecimalField(decimal_places=0, blank=False)
+
+class Team(models.Model):
+	name = models.CharField(max_length=120, blank=False)
