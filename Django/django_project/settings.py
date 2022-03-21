5f43813a3f79e56bda2e8e6d065b2b49dadee4f7
Fred Pan <fred.pan@bc.edu>
2022-03-21 16:07:43 -0400

    added crispy forms to settings

--- Django/django_project/settings.py
+++ Django/django_project/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UsersConfig'
+    'users.apps.UsersConfig',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
