744065090c87f622619cb5580367348c69e4c32f
yufan yang <yyf91699@gmail.com>
2022-03-21 16:43:07 -0400

    addedm courses

---
+++ Django/courses/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Course
+
+class CourseForm(forms.ModelForm):
+	name = forms.CharField(widget=forms.TextInput(attrs={"id":"inputCourseName"}))
+	semester = forms.CharField(widget=forms.TextInput(attrs = {"id":"inputSemester"}))
+	year = forms.DecimalField(widget=forms.TextInput(attrs = {"id":"inputSemester"}))
+	code = forms.CharField(widget=forms.TextInput(attrs={"id":"inputCode"}))
+	num_teams = forms.DecimalField(widget=forms.TextInput(attrs={"id":"inputNumTeams"}))
+	class Meta:
+		model = Course
+		fields = [
+			'name',
+			'semester',
+			'year',
+			'code',
+			'num_teams'
+		]
---
+++ Django/courses/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Course(models.Model):
+	name = models.CharField(max_length=120, blank=False) 
+	semester = models.CharField(max_length=20)
+	year = models.DecimalField(decimal_places=0, blank=False)
+	code = models.CharField(max_length=30, blank=False)
+	num_teams = models.DecimalField(decimal_places=0, blank=False)
