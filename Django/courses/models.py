ddf404d210f2c228833f0f93a267badcd3ec9117
yufan yang <yyf91699@gmail.com>
2022-03-22 01:28:26 -0400

    fixed courses models.py indentation. Added views.py

--- Django/courses/models.py
+++ Django/courses/models.py
@@ -8,20 +8,20 @@ class Course(models.Model):
         ('SUMMER', 'Summer'),
         ('WINTER', 'Winter')
     )
-	name = models.CharField(max_length=120, blank=False)
-	semester = models.CharField(max_length=1, choices=SEMESTERS)
-	year = models.DecimalField(decimal_places=0, blank=False)
-	code = models.CharField(max_length=30, blank=False)
-	course_id = models.AutoField()
+	name 		= models.CharField(max_length=120, blank=False)
+	semester 	= models.CharField(max_length=1, choices=SEMESTERS)
+	year 		= models.DecimalField(decimal_places=0, blank=False)
+	code 		= models.CharField(max_length=30, blank=False)
+	course_id 	= models.AutoField()
 
 class Team(models.Model):
-	name = models.CharField(max_length=120, blank=False)
-	course = models.CharField()
-	team_id = models.AutoField()
+	name 		= models.CharField(max_length=120, blank=False)
+	course 		= models.CharField()
+	team_id 	= models.AutoField()
 
 # Database table to map student to teams/courses
 class StudentTeam(models.Model):
 	# Holds username
-	student = models.CharField(blank=False)
-	team_id = models.DecimalField(decimal_places=0, blank=False)
+	student 	= models.CharField(blank=False)
+	team_id 	= models.DecimalField(decimal_places=0, blank=False)
 
--- Django/courses/views.py
+++ Django/courses/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def course_creation_view(request):
+
+
+	return render(request, "create_course.html",{})
