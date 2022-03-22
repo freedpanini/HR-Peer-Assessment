b8eae32a693fc912cef432f95098415b71421a39
Zach Crews <crewsz@bc.edu>
2022-03-22 00:25:09 -0400

    Update Team model

--- Django/courses/models.py
+++ Django/courses/models.py
@@ -2,11 +2,26 @@ from django.db import models
 
 # Create your models here.
 class Course(models.Model):
-	name = models.CharField(max_length=120, blank=False) 
-	semester = models.CharField(max_length=20)
+	SEMESTERS = (
+        ('FALL', 'Fall'),
+        ('SPRING', 'Spring'),
+        ('SUMMER', 'Summer'),
+        ('WINTER', 'Winter')
+    )
+	name = models.CharField(max_length=120, blank=False)
+    semester = models.CharField(max_length=1, choices=SEMESTERS)
 	year = models.DecimalField(decimal_places=0, blank=False)
 	code = models.CharField(max_length=30, blank=False)
-	num_teams = models.DecimalField(decimal_places=0, blank=False)
+	course_id = models.AutoField()
 
 class Team(models.Model):
 	name = models.CharField(max_length=120, blank=False)
+	course = models.CharField()
+	team_id = models.AutoField()
+
+# Database table to map student to teams/courses
+class StudentTeam(models.Model):
+	# Holds username
+	student = models.CharField(blank=False)
+	team_id = models.DecimalField(decimal_places=0, blank=False)
+
