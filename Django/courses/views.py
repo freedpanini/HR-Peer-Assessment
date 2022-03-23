ff6ea786a52182a8163894b4be0984fb6bb69bb7
Zach Crews <crewsz@bc.edu>
2022-03-22 22:55:27 -0400

    Added course creation functionality

--- Django/courses/views.py
+++ Django/courses/views.py
@@ -1,16 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Course, Team, StudentTeam
 from .forms import CourseForm
 # Create your views here.
 def course_creation_view(request):
-	form=CourseForm(request.POST or None)
-	if form.is_valid():
-		Course.objects.create(**form.cleaned_data)
-		form.save()
-		form=CourseForm()
-	context={
+	# form = CourseForm(request.POST or None)
+	# if form.is_valid():
+	# 	print("Form is valid")
+	# 	Course.objects.create(**form.cleaned_data)
+	# 	form.save()
+	# else:
+	# 	print("ERRORS")
+	# 	print(form.errors)
+	
+	# if request.method == "POST":
+	# 	name = request.POST.get('name')
+	# 	semester = request.POST.get('semester')
+	# 	year = request.POST.get('year')
+	# 	code = request.POST.get('code')
+	# 	num_teams = request.POST.get('num_teams')
+	# 	Course.objects.create(name=name, semester=semester, year=year, code=code)
 
-		'form':form
-	}
+	form = CourseForm()
+	if request.method == "POST":
+		form = CourseForm(request.POST)
+		if form.is_valid():
+			Course.objects.create(**form.cleaned_data)
+			return redirect('adminhome')
 
-	return render(request, "courses/course_create.html",context)
+	context = {'form':form}
+	return render(request, "courses/course_create.html", {})
