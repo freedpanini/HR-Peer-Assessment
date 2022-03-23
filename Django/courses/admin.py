d60d22ac8f94f9197d00a37e21ebf8124d50fc08
yufan yang <yyf91699@gmail.com>
2022-03-23 01:16:43 -0400

    modify create_team.html

--- Django/courses/admin.py
+++ Django/courses/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Course
+from .models import Course, Team
 
 admin.site.register(Course)
+admin.site.register(Team)
