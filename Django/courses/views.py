357ed64ba80df822f831227c27f284f1938b891a
Zach Crews <crewsz@bc.edu>
2022-03-29 13:45:46 -0400

    Prevent duplicate invitations and function to accept invitations

--- Django/courses/views.py
+++ Django/courses/views.py
@@ -31,11 +31,11 @@ def team_creation_view(request):
 	return render(request, "courses/team_create.html", context)
 
 
-def send_email(request, email, code, name):
+def send_email(request, emails, code, name):
 	send_mail(f'You\'ve been added to {name}! ',
 	f'Hello there, this is an automated message. You have been added to a new course, course code: {code}. If you do not have an account, use the link below to register! http://127.0.0.1:8000/register/ ',
 	'Software Engineer HumanResources',
-	[email],
+	emails,
 	fail_silently=False)
 	return render(request, 'courses/send_email.html',{})
 
@@ -44,9 +44,18 @@ def invite_students(request, data, course_id):
 	if emails is None or len(emails) == 0:
 		return
 	emails = emails.split(",")
-	for email in emails:
-		email = email.strip()
-		print(f"Creating invitation for student {email} for course {course_id}")
-		Invitation.objects.create(student=email, course_id=course_id)
-		print(f"Sending email to {email}")
-		send_email(request, email, data['code'], data['name'])
+	i = 0
+	while i < len(emails):
+		emails[i] = emails[i].strip()
+		# prevent duplicate invitations
+		if Invitation.objects.filter(student=emails[i], course_id=course_id).count() == 0:
+			Invitation.objects.create(student=emails[i], course_id=course_id)
+		else:
+			emails.pop(i)
+			i -= 1
+		i += 1
+	send_email(request, emails, data['code'], data['name'])
+
+def accept_invite(request, student, course_id):
+	Invitation.objects.get(student=student, course_id=course_id).delete()
+	Registration.objects.create(student=student,course_id=course_id,team_id=0)
