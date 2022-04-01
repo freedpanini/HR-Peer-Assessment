678f7baa7748a32adbc07d8cdb4947cfca059106
Fred Pan <fred.pan@bc.edu>
2022-04-01 11:13:57 -0400

    added style + template structure + started views

--- Django/assessments/views.py
+++ Django/assessments/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import PeerAssessment, Question, Answer, Submission
+from .forms import PeerAssessmentForm, QuestionForm, OptionForm
 
 # Create your views here.
+def create_assessment(request):
+    if request.method == "POST":
+        form = PeerAssessmentForm(request.POST)
+        if form.is_valid():
+            peer_assessment = form.save(commit=False)
+            peer_assessment.creator = request.user
+            peer_assessment.save()
+            return redirect("", pk=peer_assessment.id)
+    else:
+        form = PeerAssessmentForm()
+
+    return render(request, "create_assessment.html", {"form": form})
