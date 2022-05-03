1512a9246f1b06c9336daafc46ee2d865bdb0333
hannahbrooksaz <brooksha@bc.edu>
2022-05-03 10:49:54 -0400

    adding class diagram and possibly interaction diagram

--- Django/courses/views.py
+++ Django/courses/views.py
@@ -7,18 +7,7 @@ from django.conf import settings
 from users.views import get_user_invitations, get_user_registrations
 from django.contrib.auth.models import User
 import math, random
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
-# <<<<<<< Updated upstream
-# =======
-
-# >>>>>>> Stashed changes
->>>>>>> d739cf1e48e779bfd24b879bce31eb15d6f667d8
-=======
-
->>>>>>> 6262940ccba304a678c97b675400c70a20b24693
 # Create your views here.
 def course_creation_view(request):
 	form = CourseForm()
